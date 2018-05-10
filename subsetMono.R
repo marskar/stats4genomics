7d9097626a4d62a29091dda8b5c4a243dc1719e9
kuki0990 <potassiumiodide0990@gmail.com>
2018-05-10 19:34:02 -0400

    Update subsetMono.R

--- subsetMono.R
+++ subsetMono.R
@@ -6,7 +6,10 @@ mono = read.delim("~/mono.txt",sep = "\t")
 t = read.delim("~/t.txt",sep = "\t")
 subsetMono = mono[FALSE,]
 
+library(kimisc)
 for (i in 1:NROW(t))
 {
   subsetMono = rbind(subsetMono,sample.rows(subset(mono,X.Sample_characteristics_ch1 == t$X.Sample_characteristics_ch1[i] & X.Sample_characteristics_ch1.1 == t$X.Sample_characteristics_ch1.1[i] ),1))
 }
+
+write.table(subsetMono,"~/subsetMono.txt",sep="\t",quote=F,row.names=F)
